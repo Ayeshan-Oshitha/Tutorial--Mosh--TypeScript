d861f62e129c1fbf27411276e0c361cdd148d9ac
Ayeshan-Oshitha <149539603+Ayeshan-Oshitha@users.noreply.github.com>
2025-06-25 19:37:49 +0530

    Completed the Modules Section

---
+++ 007-Modules/001-Exporting-and-Importing/index.ts
@@ -0,0 +1,6 @@
+import { Circle, Square } from "./Shapes";
+
+let circle = new Circle(5);
+let square = new Square(10);
+
+console.log(circle.radius);
---
+++ 007-Modules/002-Re-exporting/Shapes/index.ts
@@ -0,0 +1,8 @@
+// RE-EXPORT EXAMPLE
+
+// Importing Circle and Square classes from their respective files
+import { Circle } from "./Circle";
+import { Square } from "./Square";
+
+// Re-exporting Circle and Square classes
+export { Circle, Square };
